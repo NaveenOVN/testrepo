eccfdd0091c9c9fddaa47e74f0eca7888309d8e7
Ubuntu <ubuntu@ip-172-31-42-219.ap-south-1.compute.internal>
2026-06-27 08:13:58 +0000

    Initial Commit

---
+++ hello.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "==================================="
+echo "Jenkins Build Started"
+echo "Current Date"
+date
+echo ""
+echo "Current User"
+whoami
+echo ""
+echo "Current Directory"
+pwd
+echo ""
+echo "Listing Files"
+ls -la
+echo ""
+echo "Build Successful"
+echo "==================================="
